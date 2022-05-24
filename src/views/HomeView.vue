8299d202bf80964b0d46ad2fbba63e05eba23ca3
bkleiner <b.kleiner@str8labs.com>
2022-05-24 23:39:11 +0200

    add form dummy to home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,55 @@
 <template>
-  <main>
-    <h1>TEST</h1>
-  </main>
+  <div class="field is-horizontal">
+    <div class="field-label is-medium">
+      <label class="label">Source</label>
+    </div>
+    <div class="field-body">
+      <div class="field">
+        <div class="control">
+          <div class="select is-medium">
+            <select>
+              <option>Select dropdown</option>
+              <option>With options</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="field is-horizontal">
+    <div class="field-label is-medium">
+      <label class="label">File</label>
+    </div>
+    <div class="field-body">
+      <div class="field">
+        <div class="file is-boxed is-medium">
+          <label class="file-label">
+            <input class="file-input" type="file" name="resume" />
+            <span class="file-cta">
+              <span class="file-icon">
+                <i class="fas fa-upload"></i>
+              </span>
+              <span class="file-label"> Choose a file… </span>
+            </span>
+          </label>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="field is-horizontal">
+    <div class="field-label">
+      <!-- Left empty for spacing -->
+    </div>
+    <div class="field-body">
+      <div class="field">
+        <div class="control">
+          <button class="button is-medium is-primary">Flash</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
