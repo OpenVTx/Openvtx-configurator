a349d7eb450f5e14d84863dbc8e1274bba7ccc1e
bkleiner <b.kleiner@str8labs.com>
2022-11-20 01:09:53 +0100

    add gh-workflow to deploy to gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,15 @@ import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+let base = "/";
+
+if (process.env.DEPLOYMENT === "gh-pages-nigthly") {
+  base = "/nightly/";
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: base,
   plugins: [vue()],
   build: {
     // sourcemap: true,
